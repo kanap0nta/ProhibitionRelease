238557ded554272e6a2d93e9cfb5d9893ed91338
kanap0nta <lisp.ai.01@gmail.com>
2021-05-14 06:49:19 +0900

    first commit

---
+++ Chrome/options.js
@@ -0,0 +1,23 @@
+// opeionts.js
+document.addEventListener("DOMContentLoaded", _ => {
+    var checkboxes = document.querySelectorAll("input[type='checkbox']");
+    for (let i = 0; i < checkboxes.length; i++) {
+        (async() => {
+            var key = checkboxes[i].id;
+            const result = await chrome.storage.local.get(key, function (items) {
+                if (void 0 !== items[key]) {
+                    checkboxes[i].checked = items[key];
+                } else {
+                    var data = {};
+                    data[key] = checkboxes[i].checked;
+                    chrome.storage.local.set(data);
+                }
+            });
+            checkboxes[i].addEventListener('change', async(e) => {
+                var data = {};
+                data[key] = e.srcElement.checked;
+                chrome.storage.local.set(data);
+            });
+        })();
+    }
+});
